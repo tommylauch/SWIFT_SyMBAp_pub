753f9df442096144cf343b3f5cc42653509611a8
tommylauch <tommylauch@uchicago.edu>
2025-10-16 01:54:48 +0800

    tuning optimized

--- symba5p/symba5p_tune.f
+++ symba5p/symba5p_tune.f
@@ -36,32 +36,33 @@ c
 c-----
 c...  Executable code 
 
-      if (start.eq.1) then
-         if (tune.eq.1) then
+      if (start.eq.1) then                                              ! before the main body
+         if (tune.eq.1) then                                            ! read file
             open(unit=10, file=fn, status='old', iostat=ierr)
             if (ierr.eq.0) then
                read(10,*) threads_file
-               threads = max(threads,threads_file)
+               threads = threads_file                                   ! use previous optimal thread count or use changed value
+               threads_max = max(threads_max,threads_file)
                close(10)
             else
                threads = threads_max
-               write(*,*) fn,' not found, start tuning from ',threads
+               write(*,*) 'No threads file found'
             endif
             call omp_set_num_threads(threads)
-            write(*,*) 'Tunning, no. of threads set to ',threads
+            write(*,*) 'Start tuning, no. of threads set to ',threads
             sc_elapseo = 0
-            threadso = threads
             tune = 2
-         else if (tune.eq.2) then
+         endif
+         if (tune.eq.2) then                                            ! start timing
             call system_clock(sc_start)
          endif
-      else
-         if (tune.eq.step+2) then
+      else                                                              ! after the main body
+         if (tune.eq.step+2) then                                       ! end timing
             call system_clock(sc_end)
             tune = 2
             sc_elapse = sc_end-sc_start
             if ((sc_elapseo.eq.0) .or. (sc_elapse.lt.sc_elapseo)) then
-               sc_elapseo = sc_elapse
+               sc_elapseo = sc_elapse                                   ! best time, continue tuning
                threadso = threads
                threads = threads-1
                if (threads .eq. 0) then
@@ -69,19 +70,17 @@ c...  Executable code
                   tune = 0
                endif
             else
-               threads = threadso
+               threads = threadso                                       ! best time with previous no. of threads
                tune = 0
             endif
             call omp_set_num_threads(threads)
-            write(*,*) 'Tunning, no. of threads set to ',threads
+            write(*,*) 'Tuning, no. of threads set to ',threads
          else
             tune = tune + 1
          endif
          if (tune.eq.0) then
-            call omp_set_num_threads(threads)
-            write(*,*) 'Tune done, no. of threads set to ',threads
+            write(*,*) 'Tune done'
             open(unit=10, file=fn, status='replace')
-            sc_elapseo = 0
             write(10,*) threads
             close(10)
          endif
